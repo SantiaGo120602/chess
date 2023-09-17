c0d8b5ecead8025c8fae20b73cb0df8060db59a0
Santiago <santyjavivas@hotmail.com>
2023-09-17 09:26:23 -0500

    working on the readme and added screenshot

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# chess - An implementation using Java Swing and the MVC pattern
+
+![Chess Board](/src/resources/images/github/board_example.png)
+
+## How to install
+
+## Required
 ## License
 
 [MIT License](LICENSE)
--- src/com/chess/view/ChessView.java
+++ src/com/chess/view/ChessView.java
@@ -1,6 +1,7 @@
 package com.chess.view;
 
 import com.chess.model.board.Board;
+import java.awt.event.ActionListener;
 
 public class ChessView {
     private MainFrame mainFrame;
@@ -11,5 +12,9 @@ public class ChessView {
 
     public MainFrame getMainFrame() {
         return mainFrame;
+    }
+
+    public void addAddBoardListener(ActionListener listener) {
+        mainFrame.getChessPanel().addActionListener(listener);
     }
 }
